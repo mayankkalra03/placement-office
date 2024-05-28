66c0864eb75ab06ea531498e84e3f9ebdc2f6214
mayankkalra03 <mayankkalravp@gmail.com>
2024-05-29 00:39:09 +0530

    modified home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -49,7 +49,7 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/job/getall" element={<Jobs />} />
           <Route path="/job/:id" element={<JobDetails />} />
           <Route path="/application/:id" element={<Application />} />
